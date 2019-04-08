1a6594e5dcd4d68ca4d835daa0baab86a158f700
Leonardo Rocha <leopdor@gmail.com>
2019-04-08 19:20:04 -0300

    Init

---
+++ src/main/java/br/com/rocha/IntegracaoContinua/IntegracaoContinuaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocha.IntegracaoContinua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoContinuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoContinuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rocha/IntegracaoContinua/IntegracaoContinuaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.rocha.IntegracaoContinua;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegracaoContinuaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
