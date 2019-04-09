5feccd84b7b07a5cd0880befd8799a9103c60c68
Leonardo Rocha <leonardo.rocha@iacit.com.br>
2019-04-09 10:23:46 -0300

    Adicinoado Postgres

--- src/main/resources/scripts/script.sql
+++ src/main/resources/scripts/script.sql
@@ -2,10 +2,6 @@
 --changeset Leonardo:1
 
 CREATE TABLE integracao.pessoa (
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	nome varchar(255) NULL,
-	CONSTRAINT pessoa_pk PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci;
+	id bigserial PRIMARY KEY,
+	nome varchar(255) NULL
+);
