6da48d7f7d193583fff5fe988e0f035559ab2a4b
Leonardo Rocha <leonardo.rocha@iacit.com.br>
2019-04-10 17:29:38 -0300

    Adicionado Script

--- src/main/resources/scripts/script.sql
+++ src/main/resources/scripts/script.sql
@@ -5,3 +5,30 @@ CREATE TABLE integracao.pessoa (
 	id bigserial PRIMARY KEY,
 	nome varchar(255) NULL
 );
+
+
+--liquibase formatted sql
+--changeset Leonardo:inserindo pessoas
+
+INSERT INTO integracao.pessoa
+(nome)
+VALUES('Carlos');
+
+INSERT INTO integracao.pessoa
+(nome)
+VALUES('Maria');
+INSERT INTO integracao.pessoa
+(nome)
+VALUES('Joana');
+INSERT INTO integracao.pessoa
+(nome)
+VALUES('Tina');
+INSERT INTO integracao.pessoa
+(nome)
+VALUES('Julia');
+INSERT INTO integracao.pessoa
+(nome)
+VALUES('Bredt');
+INSERT INTO integracao.pessoa
+(nome)
+VALUES('Jill');
